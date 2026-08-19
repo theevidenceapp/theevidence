b53ec883bd4f7b4fc316c7d96902172c17388c41
Viral Vaghela <viralvaghela8577@gmail.com>
2026-08-19 20:05:03 +0530

    [Change] session store from RAM to mongo-store

--- backend/src/config/session.ts
+++ backend/src/config/session.ts
@@ -1,5 +1,6 @@
 import session from "express-session";
 import config from "./config.js";
+import MongoStore from "connect-mongo";
 
 if (!config.SESSION_SECRET) {
   console.error("SESSION_SECRET is not set");
@@ -10,5 +11,13 @@ export const sessionConfig = session({
   secret: config.SESSION_SECRET as string,
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: false, httpOnly: true, maxAge: 24 * 60 * 60 * 100 },
+  store: MongoStore.create({
+    mongoUrl: config.MONGO_URI,
+    ttl: 14 * 24 * 60 * 60,
+  }),
+  cookie: {
+    secure: config.NODE_ENV === "production",
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 100,
+  },
 });
