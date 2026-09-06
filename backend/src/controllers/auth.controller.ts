c962e1e43cef91cf43824eacfdca6035e7aaa4a2
Viral Vaghela <viralvaghela8577@gmail.com>
2026-09-06 10:38:48 +0530

    Update auth.controller.ts

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -279,7 +279,8 @@ export const logout = async (req: Request, res: Response) => {
       return res.status(404).json(new ApiError(404, "User not found"));
     }
 
-    userExists.accessToken = undefined;
+    userExists.accessToken = "";
+
     await userExists.save();
 
     res.clearCookie("refreshToken", {
